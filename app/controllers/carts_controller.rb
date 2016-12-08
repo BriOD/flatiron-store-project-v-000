68d12bfd2ed4b789a87f005f5ab0266ff5e79bd6
BriOD <briodonoghue@gmail.com>
2016-12-08 16:15:47 -0500

    Done.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,32 @@
 class CartsController < ApplicationController
+  def show
+    @cart = current_user.current_cart
+  end
+
+  def checkout
+    update_inventory
+    current_user.remove_cart
+    redirect_to cart_path(current_user)
+    # @cart = Cart.find params[:id]
+    # # binding.pry
+    # @current_cart = current_user.current_cart
+    #   current_user.current_cart.line_items.each do |line_item|
+    #     line_item.item.inventory -= line_item.quantity
+    #     line_item.item.save
+    #   end
+    # @current_cart.status = nil
+    # redirect_to cart_path(@cart)
+  end
+
+
+  private
+
+  def update_inventory
+    current_user.current_cart.line_items.each do |item|
+      @item = Item.find(item.item_id)
+      @item.inventory -= item.quantity
+      @item.save
+    end
+  end
 
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,30 @@
 class Cart < ActiveRecord::Base
+  belongs_to :user
+  has_many :line_items
+  has_many :items, through: :line_items
+
+  def total
+    total = 0
+    self.line_items.each do |line_item|
+      total += line_item.item.price * line_item.quantity
+    end
+    total
+  end
+
+  def add_item(item_id)
+      # 3) Cart#add_item updates existing line_item instead of making new when adding same item
+      # check to see if line_item already exist, if it does add to quantity
+    line_item_exists = self.line_items.find_by(item_id: item_id)
+    if line_item_exists
+      line_item_exists.quantity +=1
+      line_item_exists
+    else
+      self.line_items.build(item_id: item_id)
+    end
+  end
+
+
+
+
 
 end
